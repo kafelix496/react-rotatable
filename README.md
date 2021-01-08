c58655dcd881b3f06e00e3fda511aa7f36e41ede
Kevin <41712713+kafelix496@users.noreply.github.com>
2021-01-08 05:59:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+![](https://badgen.net/npm/v/@kafelix496/react'ts-library-template)
+![](https://badgen.net/bundlephobia/minzip/@kafelix496/react-ts-library-template)
+![](https://badgen.net/npm/dt/@kafelix496/react-ts-library-template)
+![](https://img.shields.io/badge/license-MIT-blue.svg)
+
+# react-ts-library-template
+React ( Typescript ) library template
+
+## Step to start !
+- npm install
+- node scrtips/initConfig.js
+
+## Lint
+```
+npm run lint
+```
+
+## Unit Test
+```
+npm run test
+```
+
+## Build
+```
+npm run build
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+const common = (isProd) => ({
+  plugins: [
+    resolve(),
+    babel({
+      babelrc: true,
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled'
+    }),
+    isProd && terser(),
+    typescript({
+      clean: true,
+      useTsconfigDeclarationDir: true,
+      tsconfig: './tsconfig.bundle.json'
+    })
+  ]
+})
+
+const results = (umdName, external) => [
+  {
+    input: `src/index.ts`,
+    external: Object.keys(external),
+    output: {
+      file: pkg.module,
+      format: 'es'
+    }
+  },
+  {
+    input: `src/index.ts`,
+    external: Object.keys(external),
+    output: {
+      file: pkg.main,
+      format: 'cjs'
+    }
+  },
+  {
+    input: `src/index.ts`,
+    external: Object.keys(external),
+    output: {
+      file: pkg.unpkg,
+      format: 'umd',
+      name: umdName,
+      globals: external
+    }
+  }
+]
+
+export default results('kaReactTs', { react: 'React' }).map((object) => ({
+  ...common(true),
+  ...object
+}))
